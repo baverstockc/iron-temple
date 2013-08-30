315240cd6747d3b3364fb8f4a258e19dd1897002
jonesl <luke.jones@advancedcomputersoftware.com>
2013-08-30 13:33:04 +0100

    Add exercise functionality to new routine page.

--- app/iron-temple/03-controllers/userSetupController.js
+++ app/iron-temple/03-controllers/userSetupController.js
@@ -1,7 +1,38 @@
 angular.module("ironTemple").controller("userSetupController", function($scope, $location, userService){
 	$scope.username = userService.getCurrentUser();
+	$scope.routine = [];
+	$scope.exerciseName = "";
+	$scope.exerciseQuantity;
+	$scope.exerciseUnit = "";
 
 	$scope.newRoutine = function() {
 		$location.path("/NewRoutine");
 	};
+
+	$scope.addExercise = function() {
+		var newExercise = {
+			Name: $scope.exerciseName,
+			Quantity: $scope.exerciseQuantity,
+			Unit: $scope.exerciseUnit,
+			Sets: $scope.exerciseSets
+		};
+		$scope.routine.push(newExercise);
+		resetExercise();
+	};
+
+	$scope.showSets = function() {
+		if ($scope.exerciseUnit === "Reps") { 
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	function resetExercise() {
+		$scope.exerciseName = "";
+		$scope.exerciseQuantity = 0;
+		$scope.exerciseUnit = "";
+		$scope.exerciseSets = 0;
+	}
 });
